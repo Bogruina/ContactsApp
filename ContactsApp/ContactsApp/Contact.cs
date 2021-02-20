6d77ea3fad19e89a74bc846ae39774c5b9827849
Bogruina <57595644+Bogruina@users.noreply.github.com>
2021-02-20 18:25:17 +0700

    Реализован класс PhoneNumber, прописаны его поля, свойства и методы. Реализован класс Validator, который содержит различные проверки на валидность данных

---
+++ ContactsApp/ContactsApp/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    public class Contact
+    {
+        private string _sureName;
+        private string _name;
+        private PhoneNumber _phoneNumber;
+        private DateTime _dateOfBirth;
+        private string _email;
+        private string _idVk;
+
+
+    }
+}
---
+++ ContactsApp/ContactsApp/Validator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Класс обобщает методы проверки на валидность различных данных
+    /// </summary>
+    public static class Validator
+    {
+        /// <summary>
+        ///  Метод осуществляет проверку введенного номера телефона на соответствие требованиям
+        /// </summary>
+        /// <param name="phoneNumber"> Принимает введенный номер телефона </param>
+       public static void ValidationPhoneNumber(string phoneNumber)
+       {
+           foreach (var var in phoneNumber)
+           {
+
+               if (!char.IsDigit(var))
+               {
+                   throw new ArgumentException("Строка должна содержать только цифры");
+               }
+           }
+
+            int i = 0;
+            foreach (var b in phoneNumber)
+            {
+                i++;
+            }
+
+            if (i > 11 || i < 11) 
+            {
+                throw new ArgumentException("Номер должен состоять ровно из 11 цифр");
+            }
+
+            if (phoneNumber[0] != '7')
+            {
+                throw new ArgumentException("Код страны должен равнятся 7");
+            }
+       }
+
+
+    }
+}
