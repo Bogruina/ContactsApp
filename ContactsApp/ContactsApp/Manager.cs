dd6ea56fa7ff43452059898ac8abb3941adc532a
Bogruina <57595644+Bogruina@users.noreply.github.com>
2021-02-23 14:24:47 +0700

    Реализован класс Manager(сериализация и десериализация)

--- ContactsApp/ContactsApp/Manager.cs
+++ ContactsApp/ContactsApp/Manager.cs
@@ -10,18 +10,32 @@ namespace ContactsApp
 {
     public static class Manager
     {
-        private const string _filePath = @"C:\Users\aleks\OneDrive\Документы\ContactsApp";
+        private const string FilePath = @"C:\ContactsApp\ContactsApp.json";
 
-        public static void SaveToFile(Contact data, string filename)
+        public static void SaveToFile(Contact data)
         {
-            //JsonSerializer serializer = new JsonSerializer();
-            //serializer.Serialize(filename,data);
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(FilePath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                
+                serializer.Serialize(writer, data);
+            }
+           
 
         }
 
-        public static Contact LoadFromFile(string filename)
+        public static Contact LoadFromFile()
         {
-
+            Contact contact = null;
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamReader sr = new StreamReader(FilePath))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                //Вызываем десериализацию и явно преобразуем результат в целевой тип данных
+                contact = (Contact)serializer.Deserialize<Contact>(reader);
+                return contact;
+            }
         }
     }
 }
--- ContactsAppUI/ContactsAppUI/Program.cs
+++ ContactsAppUI/ContactsAppUI/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ContactsApp;
+using System.IO;
 
 namespace ContactsAppUI
 {
@@ -18,13 +19,19 @@ namespace ContactsAppUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            
+            Manager.LoadFromFile();
+
 
             PhoneNumber phone = new PhoneNumber();
             phone.NumberPhone = "79964156633";
-            Contact contact = new Contact("Petrov","Ivan","799964156633",
+            Contact contact = new Contact("Petrov","Ivan","79964156633",
                 new DateTime(2000,08,12),"bogruina@gmail.com","id121212");
-           
+            TextBox textBox = new TextBox();
+            textBox.Text = contact.Email;
+
+            Manager.SaveToFile(contact);
+
+
         }
     }
 }
