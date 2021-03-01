55a5adf2d7d1937297c150ee83c1c541e8d0d6fb
Bogruina <57595644+Bogruina@users.noreply.github.com>
2021-03-01 18:38:26 +0700

    Добавлены xml комментарии

--- ContactsApp/ContactsApp/Contact.cs
+++ ContactsApp/ContactsApp/Contact.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace ContactsApp
