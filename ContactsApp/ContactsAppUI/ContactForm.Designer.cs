04544fbfcc1ccc55efa2d1d8be47a2542dd5811b
Bogruina <57595644+Bogruina@users.noreply.github.com>
2021-03-13 10:10:04 +0700

    Переименована форма

--- ContactsApp/ContactsAppUI/ContactForm.Designer.cs
+++ ContactsApp/ContactsAppUI/ContactForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ContactsAppUI
 {
-    partial class AddEditContactForm
+    partial class ContactForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@ namespace ContactsAppUI
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEditContactForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContactForm));
             this.label1 = new System.Windows.Forms.Label();
             this.BirthdayDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
